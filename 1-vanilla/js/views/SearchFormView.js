f9f9ef31af25949858f6b234f6ad7230c1be98ef
MULEE-PO <lmo9903@gmail.com>
2025-05-22 15:00:36 +0900

    추천 검색어 3 실습 정답

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -46,7 +46,10 @@ export default class SearchFormView extends View {
     this.emit("@reset");
   }
 
-  show(keyword) {
-    this.inputElement.value = keyword;
+  show(value = "") {
+    this.inputElement.value = value;
+    this.showResetButton(this.inputElement.value.length > 0);
+
+    super.show();
   }
 }
